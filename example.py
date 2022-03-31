fad82caeeb0f86c459a690c13d38a04512ff5b80
ScientiaFelis <skogskatt8@gmail.com>
2022-03-31 10:19:18 +0200

    dd new py fle

---
+++ example.py
@@ -0,0 +1,18 @@
+
+def add(a,b):
+    return a+b
+
+def test_add():
+    assert add(2,3) == 5
+    assert add('space', 'ship') == 'spaceship'
+    #assert add(-1,4) == 5
+
+def substract(a,b):
+    return a+b #error to fix
+
+
+# uncomment the following test in step 5
+#def test_subtract():
+#    assert subtract(2, 3) == -1
+
+
