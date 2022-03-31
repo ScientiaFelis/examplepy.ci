aad1b611414d5f899794346fd5df4ac1caf6b832
ScientiaFelis <skogskatt8@gmail.com>
2022-03-31 10:25:26 +0200

    dd substraction test

--- example.py
+++ example.py
@@ -12,7 +12,7 @@ def substract(a,b):
 
 
 # uncomment the following test in step 5
-#def test_subtract():
-#    assert subtract(2, 3) == -1
+def test_subtract():
+    assert subtract(2, 3) == -1
 
 
